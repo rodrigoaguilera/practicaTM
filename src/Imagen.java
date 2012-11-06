84ff182d56d8a2f4dc23f6ba806e62862ad6189c
Rodrigo Aguilera <hi@rodrigoaguilera.net>
2012-11-06 18:08:14 +0100

    ya lee el zip y lo guarda en memoria como buffered images

--- src/Imagen.java
+++ src/Imagen.java
@@ -1,5 +1,7 @@
 
+import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import javax.swing.JPanel;
 
 /*
  * To change this template, choose Tools | Templates
@@ -13,6 +15,8 @@ import java.awt.image.BufferedImage;
 public class Imagen {
     private BufferedImage bi;
     private String name;
+    private int ancho;
+    private int alto;
     public Imagen(BufferedImage bi, String name){
         this.bi=bi;
         this.name=name;
@@ -45,4 +49,36 @@ public class Imagen {
     public void setName(String name) {
         this.name = name;
     }
+    
+    /**
+     * @return the ancho
+     */
+    public int getAncho() {
+        return ancho;
+    }
+
+    /**
+     * @param ancho the ancho to set
+     */
+    public void setAncho(int ancho) {
+        this.ancho = ancho;
+    }
+
+    /**
+     * @return the alto
+     */
+    public int getAlto() {
+        return alto;
+    }
+
+    /**
+     * @param alto the alto to set
+     */
+    public void setAlto(int alto) {
+        this.alto = alto;
+    }
+    public void draw(Graphics g) {       
+       g.drawImage(bi, getAncho(), getAlto(), null);
+    }
+
 }
